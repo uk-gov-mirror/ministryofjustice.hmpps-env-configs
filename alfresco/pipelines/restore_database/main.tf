c6e4a339b4bdb59badb3099275325648f43567f0
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-07-28 11:18:54 +0100

    Migrated restore db pipelines from ansible to terraform

---
+++ alfresco/pipelines/restore_database/main.tf
@@ -0,0 +1,10 @@
+locals {
+  prefix           = "alf-restore-database"
+  artefacts_bucket = data.terraform_remote_state.common.outputs.codebuild_info["artefacts_bucket"]
+  pipeline_bucket  = data.terraform_remote_state.common.outputs.codebuild_info["pipeline_bucket"]
+  iam_role_arn     = data.terraform_remote_state.common.outputs.codebuild_info["iam_role_arn"]
+  projects = {
+    terraform = data.terraform_remote_state.base.outputs.projects["terraform"]
+    ansible   = "hmpps-eng-builds-ansible3"
+  }
+}
