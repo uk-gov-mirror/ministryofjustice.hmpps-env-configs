ab669600a49f88c7619a8ce36cbf990408b85455
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-10-01 14:45:21 +0100

    Update alfresco pipelines

--- alfresco/pipelines/backups/infrastructure.tf
+++ alfresco/pipelines/backups/infrastructure.tf
@@ -1,46 +1,23 @@
-module "alfresco-dev" {
-  source           = "../../modules/pipelines/dev/git"
+module "backup-pipelines" {
+  source           = "../../modules/pipelines/backups"
   artefacts_bucket = local.artefacts_bucket
   pipeline_bucket  = local.pipeline_bucket
   prefix           = local.prefix
   iam_role_arn     = local.iam_role_arn
   repo_name        = "hmpps-delius-alfresco-shared-terraform"
   repo_branch      = "develop"
-  environments     = ["alfresco-dev"]
-  tags             = var.tags
-  projects         = local.projects
-}
-
-module "integration-envs" {
-  source           = "../../modules/pipelines/dev/s3"
-  artefacts_bucket = local.artefacts_bucket
-  pipeline_bucket  = local.pipeline_bucket
-  prefix           = local.prefix
-  iam_role_arn     = local.iam_role_arn
-  repo_name        = "hmpps-alfresco-infra-versions"
-  repo_branch      = "develop"
-  environments     = ["delius-core-dev", "delius-int"]
-  tags             = var.tags
-  projects         = local.projects
-}
-
-module "protected-envs" {
-  source           = "../../modules/pipelines/prompted/s3"
-  artefacts_bucket = local.artefacts_bucket
-  pipeline_bucket  = local.pipeline_bucket
-  prefix           = local.prefix
-  iam_role_arn     = local.iam_role_arn
-  repo_name        = "hmpps-alfresco-infra-versions"
-  repo_branch      = "develop"
   environments = [
-    "delius-training-test",
-    "delius-training",
-    "delius-test",
-    "delius-po-test1",
-    "delius-stage",
-    "delius-pre-prod",
-    "delius-perf",
-    "delius-prod"
+    # "delius-training-test",
+    # "delius-training",
+    # "delius-test",
+    # "delius-po-test1",
+    # "delius-stage",
+    # "delius-pre-prod",
+    # "delius-perf",
+    # "delius-prod",
+    # "delius-core-dev",
+    # "delius-int",
+    "alfresco-dev"
   ]
   tags     = var.tags
   projects = local.projects
--- alfresco/pipelines/backups/main.tf
+++ alfresco/pipelines/backups/main.tf
@@ -1,11 +1,8 @@
 locals {
-  git_src_envs = [
-    "alfresco-dev"
-  ]
-  prefix           = "alf-infra-build"
+  prefix           = "alf-backup-tasks"
   artefacts_bucket = data.terraform_remote_state.common.outputs.codebuild_info["artefacts_bucket"]
   pipeline_bucket  = data.terraform_remote_state.common.outputs.codebuild_info["pipeline_bucket"]
-  iam_role_arn = data.terraform_remote_state.common.outputs.codebuild_info["iam_role_arn"]
+  iam_role_arn     = data.terraform_remote_state.common.outputs.codebuild_info["iam_role_arn"]
   projects = {
     terraform = data.terraform_remote_state.base.outputs.projects["terraform"]
     ansible   = "hmpps-eng-builds-ansible3"
