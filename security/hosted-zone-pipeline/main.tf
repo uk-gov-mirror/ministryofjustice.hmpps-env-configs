ec7f8be71722a687a51ff52ed693719ec7976cae
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-10-13 16:37:40 +0100

    Added hosted zone pipeline

---
+++ security/hosted-zone-pipeline/main.tf
@@ -0,0 +1,13 @@
+####################################################
+# Locals
+####################################################
+
+locals {
+  artefacts_bucket = data.terraform_remote_state.common.outputs.codebuild_info["artefacts_bucket"]
+  pipeline_bucket  = data.terraform_remote_state.common.outputs.codebuild_info["pipeline_bucket"]
+  iam_role_arn     = data.terraform_remote_state.common.outputs.codebuild_info["iam_role_arn"]
+  projects = {
+    ansible2 = "hmpps-eng-builds-ansible2"
+  }
+  environments = ["alfresco-dev"]
+}
