3b80ea44a61b5cb16c14a5def29729ad50d8d6c2
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-11-09 11:01:40 +0000

    Am removed module code

--- eng-dev-webhook-events/eng-webhooks.tf
+++ eng-dev-webhook-events/eng-webhooks.tf
@@ -1,22 +1,3 @@
-#--------------------------------------------------------------------
-# Lambda webhook handler
-#--------------------------------------------------------------------
-# module "eng-dev-webhooks" {
-#   source                       = "./modules/eng-dev-webhooks"
-#   artifact_bucket              = local.artefacts_bucket
-#   github_oauth_token_ssm_param = local.github_oauth_token_ssm_param
-#   iam_role_codebuild           = local.iam_role_arn
-#   iam_role_codepipeline        = local.iam_role_arn
-#   name                         = local.name
-#   tags                         = var.tags
-#   lambda_map = {
-#     webhook_handler_key = local.webhook_handler_key
-#     webhook_events_key  = local.webhook_events_key
-#     event_bus_name      = "default"
-#     event_bus_source_id = "eng.ci.webhooks"
-#   }
-# }
-
 resource "aws_security_group" "webhook" {
   name        = local.name
   description = "security group for ${local.name}"
