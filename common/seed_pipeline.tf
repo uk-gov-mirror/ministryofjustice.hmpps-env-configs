3523547c1db049c5ca5c2364b9bb1d32b233b83b
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2020-11-26 18:12:29 +0000

    Added mis pipeline to seed pipeline

--- common/seed_pipeline.tf
+++ common/seed_pipeline.tf
@@ -427,6 +427,33 @@ resource "aws_codepipeline" "pipeline" {
         )
       }
     }
+    action {
+      name            = "MisInfrastructure"
+      input_artifacts = ["code"]
+      category        = "Build"
+      owner           = "AWS"
+      provider        = "CodeBuild"
+      version         = "1"
+      run_order       = 2
+      configuration = {
+        ProjectName   = aws_codebuild_project.pipelines.id
+        PrimarySource = "code"
+        EnvironmentVariables = jsonencode(
+          [
+            {
+              "name" : "COMPONENT",
+              "value" : "mis/pipelines/infrastructure",
+              "type" : "PLAINTEXT"
+            },
+            {
+              "name" : "TASK",
+              "value" : "terraform",
+              "type" : "PLAINTEXT"
+            }
+          ]
+        )
+      }
+    }
   }
   stage {
     name = "VCMS"
