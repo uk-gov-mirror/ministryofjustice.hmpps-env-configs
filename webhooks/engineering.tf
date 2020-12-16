4d1d624428e2b11892d1822c7e373625a6166e37
smjmoj <smjmoj@gmail.com>
2020-12-16 16:57:13 +0000

    dded webhook event foe hmpps base packer repo

--- webhooks/engineering.tf
+++ webhooks/engineering.tf
@@ -25,3 +25,17 @@ module "create-pipelines-eng-dev" {
     webhook_secret_key  = local.webhook_secret_key
   }
 }
+
+module "create-pipelines-hmpps-base-packer" {
+  source = "./modules/events-target"
+  event_target_map = {
+    name                = "create-pipelines-hmpps-base-packer"
+    repository          = "hmpps-base-packer"
+    source_key          = "0"
+    event_source        = local.webhook_info["dipatcher_bus_source_id"]
+    event_log_group_arn = local.webhook_info["event_log_group_arn"]
+    lambda_handler_arn  = local.webhook_info["lambda_handler_arn"]
+    webhook_invoke_url  = local.webhook_info["webhook_invoke_url"]
+    webhook_secret_key  = local.webhook_secret_key
+  }
+}
