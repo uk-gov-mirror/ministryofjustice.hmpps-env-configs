40cc237b45ba6c1ed699921536caabb88c8553ad
enyambudzi <e.nyambudzi@googlemail.com>
2020-12-18 15:24:43 +0000

    add vcms app webhook (#68)

--- webhooks/vcms.tf
+++ webhooks/vcms.tf
@@ -11,3 +11,17 @@ module "vcms-build-infrastructure" {
     webhook_secret_key  = local.webhook_secret_key
   }
 }
+
+module "vcms-build-application" {
+  source = "./modules/events-target"
+  event_target_map = {
+    name                = "vcms-build-app"
+    repository          = "hmpps-vcms"
+    source_key          = "0"
+    event_source        = local.webhook_info["dipatcher_bus_source_id"]
+    event_log_group_arn = local.webhook_info["event_log_group_arn"]
+    lambda_handler_arn  = local.webhook_info["lambda_handler_arn"]
+    webhook_invoke_url  = local.webhook_info["webhook_invoke_url"]
+    webhook_secret_key  = local.webhook_secret_key
+  }
+}
