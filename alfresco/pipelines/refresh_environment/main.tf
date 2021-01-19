b1abf1611d171dabd202d516227daa5ee06fc371
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-01-19 09:42:58 +0000

    Adjusted timeouts

--- alfresco/pipelines/refresh_environment/main.tf
+++ alfresco/pipelines/refresh_environment/main.tf
@@ -8,9 +8,13 @@ locals {
   prefix           = "alf-refresh-tasks"
   artefacts_bucket = data.terraform_remote_state.common.outputs.codebuild_info["artefacts_bucket"]
   pipeline_bucket  = data.terraform_remote_state.common.outputs.codebuild_info["pipeline_bucket"]
+  cache_bucket     = data.terraform_remote_state.common.outputs.codebuild_info["build_cache_bucket"]
+  iam_role_arn     = data.terraform_remote_state.common.outputs.codebuild_info["iam_role_arn"]
+  log_group_name   = data.terraform_remote_state.common.outputs.codebuild_info["log_group"]
   projects = {
-    terraform = data.terraform_remote_state.base.outputs.projects["terraform"]
-    ansible   = "hmpps-eng-builds-ansible3"
+    terraform = data.terraform_remote_state.common.outputs.codebuild_projects["terraform_apply"]
+    ansible   = data.terraform_remote_state.common.outputs.codebuild_projects["ansible"]
+    version   = data.terraform_remote_state.common.outputs.codebuild_projects["terraform_version"]
   }
   tags = data.terraform_remote_state.common.outputs.tags
 }
--- common/codebuild.tf
+++ common/codebuild.tf
@@ -1,21 +1,21 @@
 # main terraform project
 module "terraform_package" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-package"
-  description    = "${local.common_name}-terraform-package"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/package.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-package"
+  description        = "${local.common_name}-terraform-package"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/package.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["ansible3_image"]
+  build_image                 = var.code_build["ansible3_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -26,22 +26,22 @@ module "terraform_package" {
 }
 
 module "terraform_version" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-version"
-  description    = "${local.common_name}-terraform-version"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/version.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-version"
+  description        = "${local.common_name}-terraform-version"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/version.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["terraform_image"]
+  build_image                 = var.code_build["terraform_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -52,22 +52,22 @@ module "terraform_version" {
 }
 
 module "terraform_version_ssm" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-version-ssm"
-  description    = "${local.common_name}-terraform-version-ssm"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/version-ssm.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-version-ssm"
+  description        = "${local.common_name}-terraform-version-ssm"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/version-ssm.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["terraform_image"]
+  build_image                 = var.code_build["terraform_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -78,22 +78,22 @@ module "terraform_version_ssm" {
 }
 
 module "terraform_package_ssm" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-ssm-package"
-  description    = "${local.common_name}-terraform-ssm-package"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/package-ssm.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-ssm-package"
+  description        = "${local.common_name}-terraform-ssm-package"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/package-ssm.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["ansible3_image"]
+  build_image                 = var.code_build["ansible3_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -104,22 +104,22 @@ module "terraform_package_ssm" {
 }
 
 module "terraform_plan" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-plan"
-  description    = "${local.common_name}-terraform-plan"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/plan.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-plan"
+  description        = "${local.common_name}-terraform-plan"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/plan.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["terraform_image"]
+  build_image                 = var.code_build["terraform_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -130,22 +130,22 @@ module "terraform_plan" {
 }
 
 module "terraform_apply" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-apply"
-  description    = "${local.common_name}-terraform-apply"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/apply.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-apply"
+  description        = "${local.common_name}-terraform-apply"
+  build_timeout      = "30"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/apply.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = var.code_build["terraform_image"]
+  build_image                 = var.code_build["terraform_image"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
@@ -156,22 +156,22 @@ module "terraform_apply" {
 }
 
 module "ansible_utils" {
-  source = "../modules/codebuild-project"
-  name           = "${local.common_name}-terraform-ansible"
-  description    = "${local.common_name}-terraform-ansible"
-  build_timeout  = "30"
-  service_role   = aws_iam_role.codebuild.arn
-  tags = local.tags
-  log_group = module.create_loggroup.loggroup_name
-  buildspec = templatefile("./templates/terraform/ansible.yml.tpl", {})
+  source             = "../modules/codebuild-project"
+  name               = "${local.common_name}-terraform-ansible"
+  description        = "${local.common_name}-terraform-ansible"
+  build_timeout      = "60"
+  service_role       = aws_iam_role.codebuild.arn
+  tags               = local.tags
+  log_group          = module.create_loggroup.loggroup_name
+  buildspec          = templatefile("./templates/terraform/ansible.yml.tpl", {})
   build_compute_type = "BUILD_GENERAL1_SMALL"
-  build_type = "LINUX_CONTAINER"
+  build_type         = "LINUX_CONTAINER"
   environment = {
-    compute_type = "BUILD_GENERAL1_SMALL"
-    type = "LINUX_CONTAINER"
+    compute_type                = "BUILD_GENERAL1_SMALL"
+    type                        = "LINUX_CONTAINER"
     image_pull_credentials_type = "SERVICE_ROLE"
   }
-  build_image = local.images["ansible3"]
+  build_image                 = local.images["ansible3"]
   image_pull_credentials_type = "SERVICE_ROLE"
   environment_variables = [
     {
