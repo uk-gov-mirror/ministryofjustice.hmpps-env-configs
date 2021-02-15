806f7a3b1845a6e6c86920d48dc735c563eced3c
Don Nyambudzi <Don.Nyambudzi@gmail.com>
2021-02-15 16:09:27 +0000

    Added bucket pol to artefact bucket

--- common/main.tf
+++ common/main.tf
@@ -11,6 +11,12 @@ locals {
   common_name = "${local.prefix}-builds"
   account_id  = data.aws_caller_identity.current.account_id
   region      = var.region
+  hmpps_account_ids = merge(
+    var.aws_engineering_account_ids,
+    var.aws_account_ids,
+    var.vcms_account_ids,
+    var.ecr_account_ids
+  )
   tags = merge(
     var.tags,
     {
